918e62fcc6906e6e43fe59c679eaee47fce1118e
Simon Lacroix <simon@miaou.be>
2017-02-23 23:42:45 +0100

    Update pin settings (#5)

--- settings.py
+++ settings.py
@@ -3,13 +3,13 @@ class Settings:
     INITIAL_THRESHOLD = 500
     INITIAL_BUFFER_SIZE = 5000
     GPIO = {
-        'RE_1_CLK': 20,
-        'RE_1_DT': 21,
+        'RE_1_CLK': 6,
+        'RE_1_DT': 5,
         'RE_2_CLK': 19,
-        'RE_2_DT': 16,
-        'LED_RED': 17,
-        'LED_GREEN': 22,
-        'LED_BLUE': 24
+        'RE_2_DT': 13,
+        'LED_RED': 21,
+        'LED_GREEN': 16,
+        'LED_BLUE': 20
     }
     SCREEN = {
         'columns': 16,
